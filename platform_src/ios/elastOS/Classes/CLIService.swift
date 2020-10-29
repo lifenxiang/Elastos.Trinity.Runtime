fc2cbbc318b277665be1f620645991a95f5fa1e5
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-10-29 08:44:11 +0800

    WIP logs in the CLI service on ios

--- platform_src/ios/elastOS/Classes/CLIService.swift
+++ platform_src/ios/elastOS/Classes/CLIService.swift
@@ -16,19 +16,19 @@ public class CLIService: NSObject, NetServiceBrowserDelegate, NetServiceDelegate
     var operationCompleted = false
     let appManager: AppManager!
     var isStarted = false;
-    
+
     override init() {
         self.appManager = AppManager.getShareInstance();
         super.init()
     }
-    
+
     static func getShareInstance() -> CLIService {
         if (CLIService.cliService == nil) {
             CLIService.cliService = CLIService();
         }
         return CLIService.cliService!;
     }
-    
+
     func start() {
         if (isStarted) {
             return;
@@ -36,72 +36,98 @@ public class CLIService: NSObject, NetServiceBrowserDelegate, NetServiceDelegate
         isStarted = true;
         searchForServices()
     }
-    
+
     func stop() {
         isStarted = false;
         stopSearching(shouldRestart: false);
     }
-    
+
     private func log(_ str: String) {
         print("CLIService - \(str)")
     }
-    
+
     private func searchForServices() {
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5), execute: {
             self.log("Searching for local CLI service...")
-            
+
             if (self.serviceBrowser == nil) {
                 self.serviceBrowser = NetServiceBrowser()
                 self.serviceBrowser!.delegate = self
             }
-            
+
             self.shouldRestartSearching = true
             self.operationCompleted = false
             self.serviceBrowser!.searchForServices(ofType: "_trinitycli._tcp.", inDomain: "")
-            
+
             DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(20), execute: {
                 self.stopSearching(shouldRestart: true)
             })
         })
     }
-    
+
     private func stopSearching(shouldRestart: Bool) {
         guard serviceBrowser != nil else {
             return;
         }
-        
+
         log("Stopping service search.")
-        
+
         self.shouldRestartSearching = shouldRestart
         serviceBrowser!.stop()
     }
-    
+
     public func netServiceBrowserDidStopSearch(_ browser: NetServiceBrowser) {
         log("Search stopped.")
-        
+
         self.serviceBrowser = nil
-        
+
         if self.shouldRestartSearching {
             // Restart searching
             self.searchForServices()
         }
     }
-    
+
     public func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
         log("Found CLI service on local network: \(service)")
-        
+        if let addresses = service.addresses {
+            for addr in addresses {
+                log("\(addr)")
+            }
+        }
+        log("\(service.domain)")
+        log("\(service.port)")
+
         services.append(service)
         service.delegate = self
         service.resolve(withTimeout:10)
 
-        self.stopSearching(shouldRestart: false)
+        //self.stopSearching(shouldRestart: false)
     }
-    
+
+    public func netService(_ sender: NetService, didNotResolve errorDict: [String : NSNumber]) {
+        log("netService didNotResolve: \(errorDict)")
+    }
+
+    public func netServiceWillResolve(_ sender: NetService) {
+        log("netServiceWillResolve")
+    }
+
     public func netServiceDidResolveAddress(_ service: NetService) {
+        log("netServiceDidResolveAddress")
+
+        log("Addresses:")
+        if let addresses = service.addresses {
+            for addr in addresses {
+                log("\(addr)")
+            }
+        }
+        log("Domain: \(service.domain)")
+        log("Port: \(service.port)")
+
         guard !operationCompleted else {
             return
         }
-        
+
         // Got a resolved service info - we can call the service to get and install our EPK
         downloadEPK(usingService: service) { epkPath in
             self.log("Requesting app manager to install the EPK")
@@ -110,12 +136,12 @@ public class CLIService: NSObject, NetServiceBrowserDelegate, NetServiceDelegate
             self.searchForServices()
         }
     }
-    
+
     private func getServiceIPAddress(_ service: NetService) -> String? {
         guard service.addresses != nil else {
             return nil
         }
-        
+
         for address in service.addresses! {
             var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
             address.withUnsafeBytes { ptr in
@@ -128,32 +154,32 @@ public class CLIService: NSObject, NetServiceBrowserDelegate, NetServiceDelegate
                 }
             }
             let ipAddress = String(cString:hostname)
-            
+
             // Check if the resolved ip address is a ipv4 address, not ipv6.
             if ipAddress.contains(":") {
                 continue
             }
-            
+
             log("Resolved CLI service IP address: \(ipAddress) port: \(service.port)")
-        
+
             return ipAddress
         }
-        
+
         return nil
     }
-    
+
     private func downloadEPK(usingService service: NetService, completion: @escaping (String)->Void) {
         // Compute the service's download EPK URL
         guard let ipAddress = getServiceIPAddress(service) else {
             log("No IP address found for the service. Aborting EPK download.")
             return
         }
-        
+
         // Watchguard to prevent downloading multiple times when IP address is resolved multiple times.
         self.operationCompleted = true
-        
+
         let serviceEndpoint = "http://\(ipAddress):\(service.port)/downloadepk"
-        
+
         // Call the service to download the EPK file
         let sessionConfig = URLSessionConfiguration.default
         let session = URLSession(configuration: sessionConfig)
@@ -167,7 +193,7 @@ public class CLIService: NSObject, NetServiceBrowserDelegate, NetServiceDelegate
                 if let statusCode = (response as? HTTPURLResponse)?.statusCode {
                     if (statusCode >= 200 && statusCode < 400) {
                         self.log("EPK file downloaded successfully with status code: \(statusCode)")
-                        
+
                         // Copy EPK file to a temp location as it's going to be deleted after download by the session.
                         let destTempUrl = FileManager.default.temporaryFileURL(fileName: "appinstall.epk")!
                         do {
@@ -175,7 +201,7 @@ public class CLIService: NSObject, NetServiceBrowserDelegate, NetServiceDelegate
                             try? FileManager.default.removeItem(at: destTempUrl)
                             // Copy the downloaded file to a file that won't be deleted by the network session
                             try FileManager.default.copyItem(at: tempLocalUrl, to: destTempUrl)
-                            
+
                             completion(destTempUrl.absoluteString)
                         }
                         catch {
@@ -195,7 +221,7 @@ public class CLIService: NSObject, NetServiceBrowserDelegate, NetServiceDelegate
         }
         task.resume()
     }
-     
+
     private func installEPK(epkPath: String) {
         DispatchQueue.main.async {
             do {
